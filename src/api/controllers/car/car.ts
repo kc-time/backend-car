34aec0ba38ec4695942d5a206fb170cdd943bf79
kelvin <kelvinkc2015@gmail.com>
2022-07-25 11:57:26 -0400

    Create Car Controller

---
+++ src/api/controllers/car/car.ts
@@ -0,0 +1,73 @@
+import { Body, Get, JsonController, Post, Delete, Param, OnUndefined, Put, HttpCode } from "routing-controllers";
+
+import { Car, CarModel } from "../../models";
+const axios= require('axios');
+
+const modelFields = {'Model Year': 'year', 'Make': 'make', 'Model': 'name'}
+
+@JsonController("/cars")
+export class CarController {
+  @Get()
+  get(): Promise<Car[]> {
+    return Car.find()
+  }
+
+  @Get('/:id')
+  @OnUndefined(404)
+  getOne(@Param('id') id: string): Promise<Car> {
+    return Car.findOne(id);
+  }
+
+  @Post()
+  @HttpCode(201)
+  @OnUndefined(404)
+  async create(@Body() body: Pick<Car, "licensePlate" | "registration" | "vin" | "model">): Promise<Car> {
+    let url = `https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVin/${body.vin}?format=json`
+    if (!body.vin || !body.licensePlate || !body.registration) return undefined
+    try {
+      const { data } = await axios.get(url);
+      const modelData = data.Results.filter(row => Object.keys(modelFields).includes(row.Variable))
+                            .map(e => {
+                            return {[modelFields[e.Variable]]: e.Value}
+                          })
+      const modelJson : CarModel = Object.assign({}, ...modelData);
+      if (modelJson.make == null || modelJson.year == null || modelJson.name == null) return undefined // check valid VIN
+
+      let model = await CarModel.findOne({
+        where: modelJson,
+      })
+      if (model == null) model = await CarModel.create(modelJson).save();
+      body.model = model
+      return Car.create(body).save();
+
+
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.log(error)
+      } else {
+        console.log(error)
+      }
+    }
+    return undefined;
+  }
+
+  @Put("/:id")
+  @OnUndefined(404)
+  async update(@Param('id') id: string, @Body() body: Car): Promise<Car> {
+    let car = await Car.findOne(id)
+    if (car == null) return undefined
+
+    car = Object.assign(car, body);
+    return car.save();
+  }
+
+  @Delete('/:id')
+  @OnUndefined(404)
+  async remove(@Param('id') id: string): Promise<Car>{
+    let car = await Car.findOne(id)
+    // return car.remove();
+    if (car == null) return undefined
+    return car.softRemove();
+  }
+
+}
